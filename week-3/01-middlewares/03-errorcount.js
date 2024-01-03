146069b33366d3d3fde980727829e37ee00002f5
Rvaindra7330 <haravinadona.ravindra@gmail.com>
2024-01-03 11:49:38 +0530

    solved problems

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,11 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use(function(err,req,res,next){
+    errorCount=errorCount+1;
+    res.status(404).send("exception occured")
+    
+  }
+)
+
 module.exports = app;
